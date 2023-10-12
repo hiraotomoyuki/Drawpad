e5271deb8d5de7c4f878821b70d579276a994726
Tomoyuki Hirao <hw22a113@oecu.jp>
2023-10-12 14:07:58 +0900

    最初のコミット

---
+++ mac_drawpad_2021_v1/DrawPad/main.cpp
@@ -0,0 +1,14 @@
+#include "DrawPad.hpp"
+#include <math.h>
+
+void DrawMain()
+{
+    Clear(kColorBlack);
+    
+    FillRect(-300, -200, 600, 400, kColorRed);
+    FillTriangle(-300, -200, -300+200*2, 200, -300, 200, kColorYellow);
+    FillTriangle(-300, -200, -300+200*1, 200, -300, 200, kColorBlue);
+    FillTriangle(-300, -200, 300, -200+133*2, 300, -200, kColorWhite);
+    FillTriangle(-300, -200, 300, -200+133*1, 300, -200, kColorGreen);
+}
+
